6df126e06f02b4af565b482894123bd7baf800dd
richardgreckijr <richardgreckijr@gmail.com>
2021-09-19 19:56:26 -0400

    Added decoding to caesar function, working on unit tests for caesar function

--- src/caesar.js
+++ src/caesar.js
@@ -3,67 +3,58 @@
 // Only add code (e.g., helper methods, variables, etc.) within the scope
 // of the anonymous function on line 6
 
-//  For the Caesar shift (example: caesar("Zebra Magazine", 3) => "cheud pdjdclqh"), the tests that you write should 
-//  test that the following is true:
-//  It returns false if the shift value is equal to 0, less than -25, greater than 25, or not present.
-//  It ignores capital letters. (For example, the results of A Message and a message should be the same.)
-//  When encoding, it handles shifts that go past the end of the alphabet. (For example, shifting z to 
-//  the right by 3 should cause the z to wrap around to the front of the alphabet, so that z becomes c.)
-//  It maintains spaces and other nonalphabetic symbols in the message, before and after encoding or decoding.
-
-
-// you can add any code you want within this function scope
-
-// if shift is === 0 || shift < -25 || shift > 25 || null || undefined;
-// if character is a z, should loop back to start of alphabet(ie. z shift once to right)
-// if character is not alphabetic, do not change.
-//  write requirements. understand req. pseudo-code based off research. check logic. 
-//  check req vs pseudo code. FIND(), SWITCH STATEMENT?
-
-const caesarModule = function caesar(input, shift, encode = true) {
-
-  // convert all entries into lowercase
-  input = input.toLowerCase();
-
-  // wrapping thru the alphabet if index goes past "z" 
-  if (shift < 0){
-    return caesarModule(input, shift + 26, encode = true);
+const caesarModule = (function () {
+  // you can add any code you want within this function scope
+  
+  function caesar(input, shift, encode = true) {
+    // converts input to lowerCase; result variable to return to user
+    let text = input.toLowerCase();
+    let result = "";
+    
+    //  condtional to check if no shift is entered, if shift is greater than 25, less than -25, or equaled to zero; return false
+    if (!shift || shift > 25 || shift < -25 || shift === 0){
+    return false;
   }
 
-  // result variable
-  let result = "";
+  // inverted shift when decoding message
+  if (encode === false) shift *= -1;
 
-  // loop thru input.length
-  for (let i=0; i < input.length; i++){
-
-    // variable to hold input's index
-    let regular = input[i];
-
-    // using the match method we compare "regular" to a regex expression; \w will match any single alphanumeric character
-    if (regular.match(/\w/i)){
-
-      // variable that holds conversion of the input into ASCII character encoding
-      let anon = input.charCodeAt(i);
-
-      // if input is greater than ("a") and less than ("z")
-       if (anon >= 97 && anon <= 122){
-
-         // convert into string, reduce down to equal to shift, modulo 26 to wrap around , readding 97 to return correct shift
-        regular = String.fromCharCode(((anon - 97 + shift) % 26) + 97);
-
+    // loop thru text.length
+    for (let i=0; i < text.length; i++) {
+      let regular = input[i];
+      
+      if (regular.match(/\w/)) {
+        // using the match method we compare "regular" to a regex expression; \w will match any single alphanumeric character
+      
+        // variable that holds conversion of the input into ASCII character encoding
+        let anon = text.charCodeAt(i) + shift;
+      
+        if(anon > 122){
+          anon = anon - 26;
+        }
+        if(anon < 97){
+          anon = anon + 26;
+        }
+        let letter = String.fromCharCode(anon);
+        result += letter;
+      } else {
+        result += regular;
       }
     }
-
-    // add data to return statement
-    result += regular;
+    console.log(result);
+    return result;
   }
-
+        
   // return to user
-  console.log(result)
-  return result;
-  
-}
+
+  return {
+    caesar,
+  };
+})();
 
 module.exports = { caesar: caesarModule.caesar };
+
+
+
 
 
--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,1 +1,72 @@
+const { expect } = require("chai");
+const { caesar } = require("../src/caesar");
+
+
+
+describe ("caesar()", () => {
+    describe("errors", () => {
+
+        it ("should return false if shift amount is equal to 0", () => {
+            const message = "hello world";
+            const shift = 0;
+            const actual = caesar(message, shift);
+            expect(actual).to.be.false;
+        });
+        
+        it("should return false if shift is less than -25", () => {
+            const message = "hello world";
+            const shift = -26;
+            const actual = caesar(message, shift);
+            expect(actual).to.be.false;
+        });
+        
+        it("should return false if shift is greater than 25", () => {
+            const message = "hello world";
+            const shift = 26;
+            const actual = caesar(message, shift);
+            expect(actual).to.be.false;
+        });
+    });
+    
+    describe("encoding", () => {
+        it("should leave spaces and other symbols as is", () => {
+            const message = "hello world";
+            const shift = 3;
+            const actual = caesar(message, shift);
+            const expected = "khoor zruog";
+            expect(actual).to.equal(expected);
+        });
+
+        it("should encode the message by shifting the letters", () => {
+            const message = "hello";
+            const shift = 3;
+            const actual = caesar(message,shift);
+            const expected = "khoor";
+            expect(actual).to.equal(expected);
+        });
+
+        it("should ignore any capital letters", () => {
+            const message = "HeLlO";
+            const shift = 3;
+            const actual = caesar(message,shift);
+            const expected = "khoor";
+            expect(actual).to.equal(expected);
+        });
+
+        it("should allow a negative shift to shift the letters to the left", () =>{
+
+        });
+
+        it ("should be able to appropriately handle letters towards the end of the alphabet", () => {
+
+        });
+    });
+})
 // Write your tests here!
+//  For the Caesar shift (example: caesar("Zebra Magazine", 3) => "cheud pdjdclqh"), the tests that you write should 
+//  test that the following is true:
+//  It returns false if the shift value is equal to 0, less than -25, greater than 25, or not present.
+//  It ignores capital letters. (For example, the results of A Message and a message should be the same.)
+//  When encoding, it handles shifts that go past the end of the alphabet. (For example, shifting z to 
+//  the right by 3 should cause the z to wrap around to the front of the alphabet, so that z becomes c.)
+//  It maintains spaces and other nonalphabetic symbols in the message, before and after encoding or decoding.
