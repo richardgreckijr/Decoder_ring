b0365e9b78b70691fba44f8fe2b6ccd0b222d588
richardgreckijr <richardgreckijr@gmail.com>
2021-09-23 21:16:05 -0400

    finished substitution cipher and all unit testing

--- src/substitution.js
+++ src/substitution.js
@@ -1,18 +1,51 @@
-// Please refrain from tampering with the setup code provided here,
-// as the index.html and test files rely on this setup to work properly.
-// Only add code (e.g., helper methods, variables, etc.) within the scope
-// of the anonymous function on line 6
 
 const substitutionModule = (function () {
-  // you can add any code you want within this function scope
-
   function substitution(input, alphabet, encode = true) {
-    // your solution code here
-  }
+    
+    input = input.toLowerCase(); // return all input to lowerCase
 
+    let ogAlphabet = "abcdefghijklmnopqrstuvwxyz ";
+
+    let result = ""; // return statement
+  
+    let doesRepeat = /(.).*\1/.test(alphabet);// uses a regex expression to check if any duplicates in alphabet;
+
+    if (!alphabet || alphabet.length !== 26) return false;// checks if user entered a alphabet, enforces 26 charLength on alphabet;
+
+    if (doesRepeat) return false; // if any duplicates return false
+
+    if(encode){
+      for(let i=0; i < input.length; i++){
+        for(let j=0; j < ogAlphabet.length; j++){   // loops over argument, and compares to traditional alphabet;
+          if (input.charAt(i) === " "){
+            result += " ";
+      }   if (input.charAt(i) === ogAlphabet.charAt(j)){ // when any matches are found, pushes encoded index into return statement;
+            result += alphabet.charAt(j); 
+      }
+    }
+  }
+  
+}
+
+if (!encode){
+  for(let i=0; i < input.length; i++){
+    for(let j=0; j < ogAlphabet.length; j++){ 
+      if (input.charAt(i) === " "){
+        result += " ";
+      } if(input.charAt(i) === alphabet.charAt(j)){   // same process as above, except pushes index of traditional alphabet;
+          result += ogAlphabet.charAt(j);
+        
+      }
+      
+    }
+  }
+}  return result.replace(/  +/g, ' ');  // return statement, replaces any extra spacing added when looping thru input; 
+
+  }
   return {
     substitution,
   };
 })();
+
 
 module.exports = { substitution: substitutionModule.substitution };
--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,1 +1,70 @@
-// Write your tests here!
+const { expect } = require("chai");
+const { substitution } = require("../src/substitution");
+
+describe ("substituion()", () => {
+    describe("error handling", () => {
+        it ("should return false if the substitution alphabet argument is missing", () =>{
+                const message = "message";
+                const actual = substitution(message);
+                expect(actual).to.be.false;
+            });
+            it ("should return false if substituion alphabet arugment does not contain unique characters", () =>{
+                const message = "message";
+                const alphabet = "thismessageisonlyletters"
+                const actual = substitution(message);
+                expect(actual).to.be.false;
+            });
+            it ("should return false if the substitution alphabet is not exactly equal to 26 characters", () =>{
+                const message = "message";
+                const alphabet = "thisistooshort"
+                const actual = substitution(message);
+                expect(actual).to.be.false;
+            });
+    });
+    describe("encoding", () => {
+        it ("should encode the message by using the substitution alphabet provided by the user", () =>{
+                const message = "message";
+                const alphabet = "plmoknijbuhvygctfxrdzeswaq";
+                const actual = substitution(message, alphabet);
+                const expected = "ykrrpik";
+                expect(actual).to.equal(expected);
+            });
+            it ("should preserve all spaces", () =>{
+                const message = "my message";
+                const alphabet = ".waeszrdxtfcygvuhbijnokmpl";
+                const actual = substitution(message, alphabet);
+                const expected = "yp ysii.rs";
+                expect(actual).to.equal(expected);
+            });
+            it ("should work with any kind of alphabet including special characters", () =>{
+                const message = "message";
+                const alphabet = ".waeszrdxtfcygvuhbijnokmpl";
+                const actual = substitution(message, alphabet);
+                const expected = "ysii.rs";
+                expect(actual).to.equal(expected);
+            });
+        });
+        describe("decoding", () => {
+            it ("should decode the message by using the substitution alphabet provided by the user", () =>{
+                    const message = "ykrrpik";
+                    const alphabet = "plmoknijbuhvygctfxrdzeswaq";
+                    const actual = substitution(message, alphabet, false);
+                    const expected = "message";
+                    expect(actual).to.equal(expected);
+                });
+                it ("should preserve all spaces", () =>{
+                    const message = "yp ysii.rs";
+                    const alphabet = ".waeszrdxtfcygvuhbijnokmpl";
+                    const actual = substitution(message, alphabet, false);
+                    const expected = "my message";
+                    expect(actual).to.equal(expected);
+                });
+                it ("should work with any kind of alphabet including special characters", () =>{
+                    const message = "ysii.rs";
+                    const alphabet = ".waeszrdxtfcygvuhbijnokmpl";
+                    const actual = substitution(message, alphabet, false);
+                    const expected = "message";
+                    expect(actual).to.equal(expected);
+                });
+            });
+});
